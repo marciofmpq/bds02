5cb3ee135f256896d4459fa865a84715c6ad418c
Marcio Felipe Queiroga <marcio.felipe@seap.ma.gov.br>
2021-12-14 17:07:22 -0300

    CityControllerIT - findAll e insert

---
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -0,0 +1,17 @@
+package com.devsuperior.bds02.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.bds02.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+	@Autowired
+	private EventService service;
+	
+	
+}
