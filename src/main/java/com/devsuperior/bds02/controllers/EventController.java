21625429bdb6e46308dda82cd6bef48530db7351
Marcio Felipe Queiroga <marcio.felipe@seap.ma.gov.br>
2021-12-20 12:24:05 -0300

    Event update Test passing

--- src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -1,10 +1,16 @@
 package com.devsuperior.bds02.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.services.EventService;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
 @RestController
 @RequestMapping(value = "/events")
@@ -13,5 +19,14 @@ public class EventController {
 	@Autowired
 	private EventService service;
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+		try {
+			dto = service.update(id, dto);
+			return ResponseEntity.ok().body(dto);
+		} catch (ResourceNotFoundException e) {	
+			return ResponseEntity.notFound().build();
+		}
+	}
 	
 }
